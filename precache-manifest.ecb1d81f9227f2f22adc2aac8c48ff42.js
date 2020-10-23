22cce205b356fcac2982b2433ba3a6aae7c8f8cb
Manjotsingh Dhanjal <manjotsinghdhanjal@gmail.com>
2020-10-24 00:29:37 +0530

    Updates

---
+++ precache-manifest.ecb1d81f9227f2f22adc2aac8c48ff42.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580f852af7e0215464d57ca25ddbf8ee",
+    "url": "/event-hub/index.html"
+  },
+  {
+    "revision": "89b76fd3ede246b650c9",
+    "url": "/event-hub/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e76444bf2e23c1636fbb",
+    "url": "/event-hub/static/css/main.890ec095.chunk.css"
+  },
+  {
+    "revision": "89b76fd3ede246b650c9",
+    "url": "/event-hub/static/js/2.5ce2649f.chunk.js"
+  },
+  {
+    "revision": "7424e8cfd3aa82d4b5dd91ea4d242f0c",
+    "url": "/event-hub/static/js/2.5ce2649f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e76444bf2e23c1636fbb",
+    "url": "/event-hub/static/js/main.929c6fe1.chunk.js"
+  },
+  {
+    "revision": "cb8f422d521dcfc7e262",
+    "url": "/event-hub/static/js/runtime-main.1dbe6a30.js"
+  },
+  {
+    "revision": "814ec8ab1394415a8c91a0afe49a5a53",
+    "url": "/event-hub/static/media/topBG.814ec8ab.png"
+  }
+]);
